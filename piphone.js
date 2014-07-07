947dff26cda845c4563bafc88c8497402ddd28c9
steven-gardiner <steven_gardiner@yahoo.com>
2014-07-07 16:28:09 -0400

    codes and queries

--- piphone.js
+++ piphone.js
@@ -11,11 +11,48 @@ piphone.dial = new piphone.mods.gpiobutton.button({name:'dial', gpiono:27});
 piphone.rotary = new piphone.mods.gpiobutton.button({name:'rotary', gpiono:17, interval:30, DOWN:1});
 piphone.onoff = new piphone.mods.gpiobutton.button({name:'switch', gpiono: 18});
 
+piphone.code = {};
+piphone.code.curr = [];
+
+piphone.rotary.on('multipress', function(spec) {
+  piphone.code.curr.push(spec.count);
+
+  if (piphone.code.unlisten) {
+    clearTimeout(piphone.code.unlisten);
+  }
+
+  var code = piphone.code.curr.join("");
+  //console.error("CODE: %j", piphone.code.curr);
+
+  switch (code) {
+    case "1178":
+      process.emit('shutdown_request');
+      break;
+    case "11": 
+      piphone.rotary.code = true;
+      break;
+  }
+
+  piphone.code.unlisten = setTimeout(function() {
+    //console.error("UNLISTEN");
+    piphone.code.curr = [];
+    piphone.rotary.code = false;
+    delete piphone.code.unlisten;
+  }, 4000);
+});
+
 piphone.onoff.on('buttonup', function() {
   piphone.onoff.enabled = true;
 });
 piphone.onoff.on('longpress', function() {
   if (! piphone.onoff.enabled) { return; }
+  process.emit('shutdown_request');
+});
+
+process.on('shutdown_request', function() {
+  piphone.mods.cp.exec('mpc stop', function(err, stdout, stderr) {    
+    console.error("STOP: %j", {err:err,stdout:stdout,stderr:stderr});    
+  });
   setTimeout(function() {
     piphone.mods.cp.exec('shutdown -h now', function(err, stdout, stderr) {    
       console.error("SHUTDOWN: %j", {err:err,stdout:stdout,stderr:stderr});    
@@ -38,6 +75,7 @@ piphone.hook.on('longpress', function() {
 
 piphone.rotary.on('multipress', function(spec) {
   console.error("ROTARY: " + spec.count);
+  if (piphone.rotary.code) { return; }
   switch (spec.count) {
     case 1:
       process.emit("volume", {volume:60});
@@ -63,7 +101,23 @@ piphone.rotary.on('multipress', function(spec) {
       process.emit("volume", {volume:100});
       process.emit("mpc", {cmd:'prev'});
       break;
+    case 7:
+      process.emit("volume", {volume:100});
+      process.emit("mpcq", {query:['guapo']});
+      break;
+    case 8:
+      process.emit("volume", {volume:100});
+      process.emit("mpcq", {query:['belafonte','matilda']});
+      break;
+    case 9:
+      process.emit("volume", {volume:100});
+      process.emit("mpcq", {query:['susanna','tanyas']});
+      break;
     case 10:
+      process.emit("volume", {volume:100});
+      process.emit("mpcq", {query:['puff']});
+      break;
+    case 11:
       var tts = piphone.mods.cp.exec('/usr/local/bin/tts');
       tts.stdin.write("how much wood would a wood chuck chuck if a woodchuck would chuck wood?\n");
       tts.stdin.end();
@@ -76,6 +130,14 @@ piphone.rotary.on('buttonpress', function(spec) {
 
 
 
+process.on('mpcq', function(spec) {
+    var mpc = piphone.mods.cp.exec(['/usr/local/bin/mpc_query',spec.query.join(".*")].concat([
+      '|',
+      'xargs mpc play'
+    ]).join(" "), function(err,stdout,stderr) {
+    console.error("DEMAND: %j", {err:err,stdout:stdout,stderr:stderr});    
+  });
+});
 process.on('mpc', function(spec) {
   piphone.mods.cp.exec(['mpc',spec.cmd].join(" "), function(err, stdout, stderr) {    
     console.error("%s: %j", spec.cmd, {err:err,stdout:stdout,stderr:stderr});    
