4dfa1da2f60256fed4e653c2356b5371ba1baebe
steven-gardiner <steven_gardiner@yahoo.com>
2014-07-30 00:18:11 -0400

    test multi

--- piphone.js
+++ piphone.js
@@ -117,9 +117,11 @@ piphone.rotary.on('multipress', function(spec) {
       process.emit("mpcq", {query:['susanna','tanyas']});
       break;
     case 10:
-      piphone.mods.cp.exec('tts puff puff paper', function(err, stdout, stderr) {    
-        console.error("SOLONG: %j", {err:err,stdout:stdout,stderr:stderr});    
-      });
+      var tts = piphone.mods.cp.exec('/usr/bin/tts');
+      tts.stdout.pipe(process.stdout);
+      tts.stderr.pipe(process.stderr);
+      tts.stdin.write('puff puff paper\n');
+      tts.stdin.end();
       process.emit("volume", {volume:100});
       process.emit("mpcq", {query:['puff']});
       break;
